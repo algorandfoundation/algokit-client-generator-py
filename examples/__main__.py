be7e5d9b0de02ac6dae65ec6b5d582d013458a73
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2023-05-19 10:15:34 +0800

    chore: fix examples linting errors

--- examples/__main__.py
+++ examples/__main__.py
@@ -1,7 +1,6 @@
-import imp
+import importlib
 import logging
 import os
-import sys
 from collections.abc import Generator
 from contextlib import contextmanager
 from pathlib import Path
@@ -21,23 +20,18 @@ def cwd(path: Path) -> Generator[None, None, None]:
         os.chdir(old_pwd)
 
 
-def main(action: str) -> None:
-    match action:
-        case "build":
-            example_dirs = filter(lambda file: file.is_dir() and "__" not in file.name, root_path.glob("*"))
-            for example in example_dirs:
-                logger.info(f"Building example {example.name}")
-                with cwd(root_path):
-                    evaluated_file = imp.load_source(example.name, f"./{example.name}/{example.name}.py")
-                app = evaluated_file.app
-                logger.info(f"  Building app {app.name}")
-                appspec = app.build()
-                logger.info(f"  Writing {example.name}/application.json")
-                (example / "application.json").write_text(appspec.to_json())
+def main() -> None:
+    example_dirs = filter(lambda file: file.is_dir() and "__" not in file.name, root_path.glob("*"))
+    for example in example_dirs:
+        logger.info(f"Building example {example.name}")
+        with cwd(root_path):
+            evaluated_file = importlib.import_module(f"examples.{example.name}.{example.name}")
+        app = evaluated_file.app
+        logger.info(f"  Building app {app.name}")
+        app_spec = app.build()
+        logger.info(f"  Writing {example.name}/application.json")
+        (example / "application.json").write_text(app_spec.to_json())
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        main(sys.argv[1])
-    else:
-        main("build")
+    main()
