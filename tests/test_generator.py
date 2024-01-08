5af0e5759ae7414022c40aa08a5e804cc0c011a3
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2024-01-08 11:17:38 +0800

    test: update tests so each example is parametrized

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,14 +1,15 @@
 import pathlib
 
+import pytest
 from algokit_client_generator import generate_client
 
 
-def test_generate_clients() -> None:
+@pytest.mark.parametrize("app", ["helloworld", "lifecycle", "state", "voting"])
+def test_generate_clients(app: str) -> None:
     examples = pathlib.Path(__file__).parent.parent / "examples"
-    for app in ["helloworld", "lifecycle", "state", "voting"]:
-        app_path = examples / app
-        app_spec = app_path / "application.json"
-        generated_path = app_path / "client_generated.py"
-        approved_path = app_path / "client.py"
-        generate_client(app_spec, generated_path)
-        assert generated_path.read_text() == approved_path.read_text()
+    app_path = examples / app
+    app_spec = app_path / "application.json"
+    generated_path = app_path / "client_generated.py"
+    approved_path = app_path / "client.py"
+    generate_client(app_spec, generated_path)
+    assert generated_path.read_text() == approved_path.read_text()
