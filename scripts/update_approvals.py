a8be469a8cafdcf0e3a30127461beb22fa0f61ff
CiottiGiorgio <giorgio.ciotti@algorand.foundation>
2025-02-05 13:19:33 +0100

    fix: simplifying update_approvals.py

--- scripts/update_approvals.py
+++ scripts/update_approvals.py
@@ -1,4 +1,5 @@
 import pathlib
+from itertools import product, chain
 
 from algokit_client_generator import generate_client
 
@@ -9,32 +10,22 @@ def to_camel_case(s: str) -> str:
 
 def update_approvals() -> None:
     artifacts = pathlib.Path(__file__).parent.parent / "examples" / "smart_contracts" / "artifacts"
-    for app in [
+    arc32_apps = [
         "duplicate_structs",
         "hello_world",
         "lifecycle",
         "minimal",
         "state",
         "voting_round",
-    ]:
-        app_path = artifacts / app
-        app_spec = app_path / f"{to_camel_case(app)}.arc32.json"
-        approved_path = app_path / f"{app}_client.py"
-        try:
-            generate_client(app_spec, approved_path)
-        except Exception as e:
-            print(f"Error generating client for {app}: {e}")
-
-    for app in [
-        "arc56_test",
+    ]
+    arc56_apps = [
         "structs",
         "nested",
-        "nfd",
-        "reti",
-        "zero_coupon_bond",
-    ]:
+    ]
+
+    for app, extension in chain(product(arc32_apps, ["arc32"]), product(arc56_apps, ["arc56"])):
         app_path = artifacts / app
-        app_spec = app_path / f"{to_camel_case(app)}.arc56.json"
+        app_spec = app_path / f"{to_camel_case(app)}.{extension}.json"
         approved_path = app_path / f"{app}_client.py"
         try:
             generate_client(app_spec, approved_path)
