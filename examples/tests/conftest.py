253f759844bc37277b509b8307cd67023e8b7949
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2025-12-02 14:38:52 +0800

    chore: remove unused functions

--- examples/tests/conftest.py
+++ examples/tests/conftest.py
@@ -1,24 +1,7 @@
-import uuid
-
 import algokit_utils
 import pytest
-from algokit_algod_client import AlgodClient
-
-
-def get_unique_name() -> str:
-    return str(uuid.uuid4()).replace("-", "")
-
 
 @pytest.fixture(scope="session")
 def algorand() -> algokit_utils.AlgorandClient:
     return algokit_utils.AlgorandClient.from_environment()
 
-
-@pytest.fixture
-def new_account(algod_client: AlgodClient) -> algokit_utils.SigningAccount:
-    return algokit_utils.get_account(algod_client, get_unique_name())
-
-
-@pytest.fixture(scope="session")
-def funded_account(algod_client: AlgodClient) -> algokit_utils.SigningAccount:
-    return algokit_utils.get_account(algod_client, get_unique_name())
